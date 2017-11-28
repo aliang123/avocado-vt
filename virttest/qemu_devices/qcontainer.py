59ad73bbc93013433dbcdb9346bafd5207aba5cd
Aihua Liang <aliang@redhat.com>
2017-11-28 15:23:05 +0800

    Add param "bus" to device "scsi-*".
    In qemu cmdline, when create device "scsi-hd", no bus defined.
    So add the param.
    
    Signed-off-by: Aihua Liang <aliang@redhat.com>

--- virttest/qemu_devices/qcontainer.py
+++ virttest/qemu_devices/qcontainer.py
@@ -1288,8 +1288,9 @@ class DevContainer(object):
                 bus += ".%d" % unit
             # If bus was not set, don't set it, unless the device is
             # a spapr-vscsi device.
-            elif _bus is None and 'spapr_vscsi' not in _hba:
-                bus = None
+            elif _bus is None:
+                if ('virtio-scsi-pci' and 'spapr_vscsi') not in _hba:
+                    bus = None
             return devices, bus, {'type': qtype, 'atype': atype}
 
         #
@@ -1511,6 +1512,8 @@ class DevContainer(object):
             devices[-1].set_param('removable', removable, bool)
             if strict_mode:
                 devices[-1].set_param('channel', 0)
+            if bus:
+                devices[-1].set_param('bus', bus)
         elif fmt == 'virtio':
             devices[-1].set_param('driver', 'virtio-blk-pci')
             devices[-1].set_param("scsi", scsi, bool)
